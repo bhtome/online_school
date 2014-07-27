e22d0a0d9ce0c82cbc5cf066fa43bae572f083ae
bhtome <bojung2011@gmail.com>
2014-07-27 16:16:57 -0700

    added list of all promotions

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,5 +1,16 @@
 class PromotionsController < ApplicationController
+
 	def index
+
+	 	response = RestClient.get "http://www.webappcamp.ws/api/v0/promotions"
+
+	 	r = JSON.parse(response)
+
+	 	@promotions = r['data']
+
+		# @x = 10
+
+
 	end
 
 end
